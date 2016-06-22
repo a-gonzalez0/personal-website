dc73061258660b69591ef72a47ae22a650f16156
a-gonzalez0 <a_gonzalez0@yahoo.com>
2016-06-22 19:12:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ var main = function() {
 };
 
 $(document).ready(function(){
+    // Modal 
     $('#myModal').on('show.bs.modal', function (e) {
             var image = $(e.relatedTarget).attr('src');
             $(".img-responsive").attr("src", image);
