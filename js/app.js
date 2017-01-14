79c514a6c7fee77e062cac79434108cacb5cc127
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-01-13 23:30:43 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
     });
     
     // Add scrollspy to <body>
-    $('body').scrollspy({target: ".navbar", offset: 50});   
+    $('body').scrollspy({target: ".navbar-nav", offset: 50});   
 
     // Add smooth scrolling on all links inside the navbar
     $("#myNavbar a").on('click', function(event) {
