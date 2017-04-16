b86cc02398f47d3e6cd60344e6b12d09b5641baa
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-16 00:12:26 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,6 +49,43 @@ $(document).ready(function(){
        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
    });
 
+    window.sr = ScrollReveal();
+        sr.reveal('.navbar', {
+          duration: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('.showcase-left', {
+          duration: 2000,
+          origin:'top',
+          distance:'300px'
+        });
+        sr.reveal('.showcase-right', {
+          duration: 2000,
+          origin:'right',
+          distance:'300px'
+        });
+        sr.reveal('.showcase-btn', {
+          duration: 2000,
+          delay: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('#testimonial div', {
+          duration: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('.info-left', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px',
+          viewFactor: 0.2
+        });
+        sr.reveal('.info-right', {
+          duration: 2000,
+          origin:'right',
+          distance:'300px',
+          viewFactor: 0.2
+        });
+    
     // Modal 
     $('#myModal').on('show.bs.modal', function (e) {
             var image = $(e.relatedTarget).attr('src');
