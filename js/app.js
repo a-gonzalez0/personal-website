ae6107eb5a7bb9cde062d9dc60e5c47858325d28
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-20 15:02:48 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,14 +122,6 @@ $(document).ready(function(){
     });
 });
 
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(40.7128, -74.0059), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-
 $(document).click(function (event) {
     var clickover = $(event.target);
     var $navbar = $(".navbar-collapse");               
