9c4afeaf49d67ed5cac3c6267c13bec34b9b170e
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-22 00:22:17 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     window.sr = ScrollReveal();
         sr.reveal('.navbar-brand', {
           duration: 2000,
-          origin:'left'
+          origin:'top'
         });
         sr.reveal('.showcase-left', {
           duration: 2000,
@@ -37,9 +37,7 @@ $(document).ready(function(){
         });
         sr.reveal('.info-left', {
           duration: 2000,
-          origin:'left',
-          distance:'300px',
-          viewFactor: 0.2
+          origin:'bottom'
         });
         sr.reveal('.info-right', {
           duration: 2000,
