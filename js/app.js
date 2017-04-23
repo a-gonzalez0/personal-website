a2eadbe1f35a5a1d48039de3cd6c9fcff418291d
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-22 20:02:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ $(document).ready(function(){
 
 $(document).click(function (event) {
     var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");               
+    var $navbar = $(".collapse");               
     var _opened = $navbar.hasClass("in");
     if (_opened === true && !clickover.hasClass("navbar-toggler")) {      
         $navbar.collapse('hide');
