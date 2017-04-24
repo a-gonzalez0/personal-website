3acad5293ed09dd23d0ed93a95e4f197a565b361
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-24 14:40:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     Navbar color change while scrolling
     $(document).scroll(function () {
-        var $nav = $('.fixed-top').offset().top;
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        var a = $('.fixed-top').offset().top;
+        a.toggleClass('scrolled', $(this).scrollTop() > a.height());
     });
 	
 //     var a = $(".fixed-top").offset().top;
