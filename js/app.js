a29dca94d2cf7de3e3d5c53558008f453d7c1122
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-04-24 23:45:28 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,6 +15,24 @@ $(document).ready(function(){
 // 	    }
 // 	});
 	
+	$(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+	
     $(document).click(function(e) {
 		if (!$(e.target).is('a')) {
 			$('.collapse').collapse('hide');	    
