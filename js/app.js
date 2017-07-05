ba2f6c3b29aa6269bf249c955b168a48e34c69de
a-gonzalez0 <a_gonzalez0@yahoo.com>
2017-07-04 23:16:03 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,26 +3,25 @@ $(document).ready(function(){
     $(document).scroll(function () {
         var $nav = $('.fixed-top');
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-	$nav.toggleClass('text-black', $(this).scrollTop() > $nav.height());
     });
 	
 	$(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
         
-        $('#back-to-top').tooltip('show');
+    $('#back-to-top').tooltip('show');
 	
     $(document).click(function(e) {
 		if (!$(e.target).is('a')) {
