8e1d395e52d96d728123d821000aff4990701b48
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2019-06-11 14:46:59 -0400

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -10,21 +10,16 @@
             s = checkTime(today.getSeconds());
         h =  h % 12 || 12;
         document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        
+        // Set AM or PM
+        var meridiem = h < 12 ? 'AM' : 'PM';
+        document.getElementById("meridiem").innerHTML = meridiem;
         t = setTimeout(function () {
             startTime()
         }, 500);
     }
 
-    function checkMeridiem() {
-    	var today = new Date(),
-            h = checkTime(today.getHours()),
-            m = checkTime(today.getMinutes()),
-            s = checkTime(today.getSeconds());
-        var meridiem = h < 12 ? 'AM' : 'PM';
-        document.getElementById("meridiem").innerHTML = meridiem;
-    }
-
-    function checkDate() {
+    function startDate() {
     	var d = new Date();
     	document.getElementById("date").innerHTML = d.toDateString();
     }
@@ -48,7 +43,6 @@
     }
     
     startTime();
-    checkMeridiem();
-    checkDate();
+    startDate();
     setBgGreet();
 })();
