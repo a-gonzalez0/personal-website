4d9d2affa546fed19d5fc6f836df9d94d9b01da5
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2019-11-24 09:54:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
         origin:'bottom',
         distance:'150px',
         viewFactor: 0.1,
-	delay: 500
+	delay: 250
     });
 
     sr.reveal('.info-bottom-3', {
@@ -81,7 +81,7 @@ $(document).ready(function(){
         origin:'bottom',
         distance:'150px',
         viewFactor: 0.1, 
-	delay: 1000
+	delay: 500
     });
 	
     // Add scrollspy to <body>
